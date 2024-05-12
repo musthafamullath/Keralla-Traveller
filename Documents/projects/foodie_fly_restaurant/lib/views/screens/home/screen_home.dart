9e2e6cd142860d5c78a4cfa725ee04b99fdbdf2e
musthafa <musthafa404601kalad@gmail.com>
2024-05-12 12:18:24 +0530

    changed sth

--- Documents/projects/foodie_fly_restaurant/lib/views/screens/home/screen_home.dart
+++ Documents/projects/foodie_fly_restaurant/lib/views/screens/home/screen_home.dart
@@ -69,93 +69,98 @@ class _ScreenHomeState extends State<ScreenHome> {
                                 style: regularGrey,
                               ),
                             )
-                          : Container(
-                            margin: const EdgeInsets.only(left: 20,right: 20,),
-                            padding: const EdgeInsets.only(top: 10,bottom: 10),
-                              height: height * 3/10,
-                              width: width * 1,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade100,
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              child: Column(
-                                children: List.generate(
-                                  state.orders.length,
-                                  (index) {
-                                    return Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 10, right: 10),
-                                      child: Card(
-                                        shadowColor: grey,
-                                        surfaceTintColor: grey,
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                  width: 1.5,
-                                                  color: Colors.grey.shade300),
-                                              borderRadius:
-                                                  BorderRadius.circular(10)),
-                                          child: Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 10, vertical: 5),
-                                            child: Column(
-                                              children: [
-                                                ListTile(
-                                                  contentPadding:
-                                                      const EdgeInsets
-                                                          .symmetric(
-                                                          horizontal: 8),
-                                                  title: Text(
-                                                    'Order id: ${state.orders[index].orderId}',
-                                                    style: regularGrey,
-                                                  ),
-                                                  subtitle: Text(
-                                                    'Order Amount: ₹ ${state.orders[index].totalPrice - state.orders[index].deliveryCharge}\nDate: ${DateFormat('d MMMM yyyy').format(DateTime.parse(state.orders[index].orderDate))}',
-                                                    style: regularGrey,
-                                                  ),
-                                                  trailing: CircleAvatar(
-                                                    backgroundColor: Colors.grey
-                                                        .withOpacity(0.3),
-                                                    radius: 24,
-                                                    child: IconButton(
-                                                      onPressed: () {
-                                                        Navigator.of(context)
-                                                            .push(
-                                                          MaterialPageRoute(
-                                                            builder: (context) =>
-                                                                ScreenOrderDetails(
-                                                              order:
-                                                                  state.orders[
-                                                                      index],
-                                                            ),
+                          : AspectRatio(
+                            aspectRatio: 1.5,
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 20,right: 20,),
+                              padding: const EdgeInsets.only(top: 10,bottom: 10),
+                                height: height * 3/10,
+                                width: width * 1,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade100,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    children: List.generate(
+                                      state.orders.length,
+                                      (index) {
+                                        return Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 10, right: 10),
+                                          child: Card(
+                                            shadowColor: grey,
+                                            surfaceTintColor: grey,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  border: Border.all(
+                                                      width: 1.5,
+                                                      color: Colors.grey.shade300),
+                                                  borderRadius:
+                                                      BorderRadius.circular(10)),
+                                              child: Padding(
+                                                padding: const EdgeInsets.symmetric(
+                                                    horizontal: 10, vertical: 5),
+                                                child: Column(
+                                                  children: [
+                                                    ListTile(
+                                                      contentPadding:
+                                                          const EdgeInsets
+                                                              .symmetric(
+                                                              horizontal: 8),
+                                                      title: Text(
+                                                        'Order id: ${state.orders[index].orderId}',
+                                                        style: regularGrey,
+                                                      ),
+                                                      subtitle: Text(
+                                                        'Order Amount: ₹ ${state.orders[index].totalPrice - state.orders[index].deliveryCharge}\nDate: ${DateFormat('d MMMM yyyy').format(DateTime.parse(state.orders[index].orderDate))}',
+                                                        style: regularGrey,
+                                                      ),
+                                                      trailing: CircleAvatar(
+                                                        backgroundColor: Colors.grey
+                                                            .withOpacity(0.3),
+                                                        radius: 24,
+                                                        child: IconButton(
+                                                          onPressed: () {
+                                                            Navigator.of(context)
+                                                                .push(
+                                                              MaterialPageRoute(
+                                                                builder: (context) =>
+                                                                    ScreenOrderDetails(
+                                                                  order:
+                                                                      state.orders[
+                                                                          index],
+                                                                ),
+                                                              ),
+                                                            );
+                                                          },
+                                                          icon: const Icon(
+                                                            CupertinoIcons
+                                                                .chevron_forward,
+                                                            size: 32,
+                                                            fill: BorderSide
+                                                                .strokeAlignCenter,
                                                           ),
-                                                        );
-                                                      },
-                                                      icon: const Icon(
-                                                        CupertinoIcons
-                                                            .chevron_forward,
-                                                        size: 32,
-                                                        fill: BorderSide
-                                                            .strokeAlignCenter,
+                                                        ),
+                                                      ),
+                                                      shape: RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius.circular(
+                                                                30),
                                                       ),
                                                     ),
-                                                  ),
-                                                  shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            30),
-                                                  ),
+                                                  ],
                                                 ),
-                                              ],
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      ),
-                                    );
-                                  },
+                                        );
+                                      },
+                                    ),
+                                  ),
                                 ),
                               ),
-                            );
+                          );
                     },
                   ),
                   kHight20,
