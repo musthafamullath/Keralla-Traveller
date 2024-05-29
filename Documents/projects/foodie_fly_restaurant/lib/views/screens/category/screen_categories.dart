1170e46e2878f171d8126f83cb8ead9d4caf5dbb
musthafa <musthafa404601kalad@gmail.com>
2024-05-29 12:23:43 +0530

    soconde hosting changes

--- Documents/projects/foodie_fly_restaurant/lib/views/screens/category/screen_categories.dart
+++ Documents/projects/foodie_fly_restaurant/lib/views/screens/category/screen_categories.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:foodie_fly_restaurant/controllers/blocs/category/category_bloc.dart';
 import 'package:foodie_fly_restaurant/utils/constants.dart';
+import 'package:foodie_fly_restaurant/utils/text_styles.dart';
 import 'package:foodie_fly_restaurant/views/screens/category/widget/action_screen_category.dart';
 
 class HomeCategoryGridviews extends StatelessWidget {
@@ -11,84 +12,71 @@ class HomeCategoryGridviews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final heightSize = MediaQuery.of(context).size.height;
-    final widthSize = MediaQuery.of(context).size.width;
     context.read<CategoryBloc>().add(FetchCategories());
 
     return BlocBuilder<CategoryBloc, CategoryState>(
       builder: (context, state) {
-        return Container(
-          margin: const EdgeInsets.only(left: 10, right: 20),
-          padding: const EdgeInsets.only(top: 20, bottom: 10),
-          height: heightSize * 3.5 / 10,
-          width: widthSize * 1,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: state.categories.isEmpty
-              ? const Center(
-                  child: SpinKitFadingCircle(
-                    color: yellow,
-                    size: 50.0, 
-                  ),
-                )
-              : GridView.builder(
-                  itemCount: state.categories.length,
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3),
-                  itemBuilder: (context, index) {
-                    return state.categories[index].iconUrl.isEmpty
-                                  ? const SpinKitFadingCircle(
-                                      color: yellow, size: 20)
-                                  :
-                    InkWell(
-                      onTap: () async {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => ActionScreenCategory(
-                              category: state.categories[index],
-                            ),
-                          ),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(1.0),
-                        child: Container(
-                          alignment: Alignment.center,
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 0.5, vertical: 0),
-                          decoration: BoxDecoration(
-                              color: Colors.grey[100],
-                              borderRadius: BorderRadius.circular(30),
-                              border: Border.all(
-                                  width: 0.9, color: grey.withOpacity(0.5))),
+        if (state.isLoading) {
+          return const Center(
+            child: SpinKitFadingCircle(
+              color: yellow,
+              size: 50.0,
+            ),
+          );
+        }
+        final categoriesToShow = state.categories.take(6).toList();
+        return state.categories.isEmpty
+            ? const Center(
+                child: Text(
+                  "No Categories Available",
+                  style: semiBoldGrey,
+                ),
+              )
+            : GridView.builder(
+                itemCount: categoriesToShow.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    ),
+                itemBuilder: (context, index) {
+                  return state.categories[index].iconUrl.isEmpty
+                      ? const SpinKitFadingCircle(color: yellow, size: 20)
+                      : InkWell(
+                          onTap: () async {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => ActionScreenCategory(
+                                  category: state.categories[index],
+                                ),
+                              ),
+                            );
+                          },
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
-                               Image.network(
-                                      state.categories[index].iconUrl,
-                                      height: heightSize * 0.8 / 10,
-                                      filterQuality: FilterQuality.high,
-                                      fit: BoxFit.fill,
-                                    ),
+                              Image.network(
+                                state.categories[index].iconUrl,
+                                height: heightSize * 1 / 10,
+                                filterQuality: FilterQuality.high,
+                                fit: BoxFit.fill,
+                              ),
                               Text(
                                 state.categories[index].name,
                                 style: const TextStyle(
-                                    fontSize: 15,
+                                    fontSize: 12,
                                     fontWeight: FontWeight.bold,
                                     color: grey),
                               ),
-                              kHight10,
+                              kHight20,
                             ],
                           ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-        );
+                        );
+                },
+              );
       },
     );
   }
--- Documents/projects/foodie_fly_restaurant/lib/views/screens/home/widgets/recent_order_widget.dart
+++ Documents/projects/foodie_fly_restaurant/lib/views/screens/home/widgets/recent_order_widget.dart
@@ -23,115 +23,101 @@ class RecentOrders extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<OrderBloc, OrderState>(
       builder: (context, state) {
-        return state is OrderLoading
-            ? const SpinKitWave(
+        if(state is OrderLoading){
+          return const SpinKitWave(
                 color: yellow,
                 size: 50.0,
-              )
-            : AspectRatio(
-                aspectRatio: 1.5,
-                child: Container(
-                  margin: const EdgeInsets.only(
-                    left: 20,
-                    right: 20,
-                  ),
-                  padding:
-                      const EdgeInsets.only(top: 10, bottom: 10),
-                  height: height * 3 / 10,
-                  width: width * 1,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: List.generate(
-                        state.orders.length,
-                        (index) {
-                          return Padding(
-                            padding: const EdgeInsets.only(
-                                left: 10, right: 10),
-                            child: Card(
-                              shadowColor: grey,
-                              surfaceTintColor: grey,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        width: 1.5,
-                                        color:
-                                            Colors.grey.shade300),
-                                    borderRadius:
-                                        BorderRadius.circular(
-                                            10)),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(
-                                          horizontal: 10,
-                                          vertical: 5),
-                                  child: Column(
-                                    children: [
-                                      ListTile(
-                                        contentPadding:
-                                            const EdgeInsets
-                                                .symmetric(
-                                                horizontal: 8),
-                                        title: Text(
-                                          'Order id: ${state.orders[index].orderId}',
-                                          style: regularGrey,
-                                        ),
-                                        subtitle: Text(
-                                          'Order Amount: ₹ ${state.orders[index].totalPrice - state.orders[index].deliveryCharge}\nDate: ${DateFormat('d MMMM yyyy').format(DateTime.parse(state.orders[index].orderDate))}',
-                                          style: regularGrey,
-                                        ),
-                                        trailing: CircleAvatar(
-                                          backgroundColor: Colors
-                                              .grey
-                                              .withOpacity(0.3),
-                                          radius: 24,
-                                          child: IconButton(
-                                            onPressed: () {
-                                              Navigator.of(
-                                                      context)
-                                                  .push(
-                                                MaterialPageRoute(
-                                                  builder:
-                                                      (context) =>
-                                                          ScreenOrderDetails(
-                                                    order: state
-                                                            .orders[
-                                                        index],
-                                                  ),
-                                                ),
-                                              );
-                                            },
-                                            icon: const Icon(
-                                              CupertinoIcons
-                                                  .chevron_forward,
-                                              size: 32,
-                                              fill: BorderSide
-                                                  .strokeAlignCenter,
-                                            ),
-                                          ),
-                                        ),
-                                        shape:
-                                            RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius
-                                                  .circular(30),
+              );
+          
+        }
+        final recentOrders =  state.orders.take(2).toList();
+        return SingleChildScrollView(
+          child: Column(
+            children: List.generate(
+              recentOrders.length,
+              (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                      left: 10, right: 10),
+                  child: Card(
+                    shadowColor: grey,
+                    surfaceTintColor: grey,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 1.5,
+                              color:
+                                  Colors.grey.shade300),
+                          borderRadius:
+                              BorderRadius.circular(
+                                  10)),
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 5),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              contentPadding:
+                                  const EdgeInsets
+                                      .symmetric(
+                                      horizontal: 8),
+                              title: Text(
+                                'Order id: ${state.orders[index].orderId}',
+                                style: regularGrey,
+                              ),
+                              subtitle: Text(
+                                'Order Amount: ₹ ${state.orders[index].totalPrice - state.orders[index].deliveryCharge}\nDate: ${DateFormat('d MMMM yyyy').format(DateTime.parse(state.orders[index].orderDate))}',
+                                style: regularGrey,
+                              ),
+                              trailing: CircleAvatar(
+                                backgroundColor: Colors
+                                    .grey
+                                    .withOpacity(0.3),
+                                radius: 24,
+                                child: IconButton(
+                                  onPressed: () {
+                                    Navigator.of(
+                                            context)
+                                        .push(
+                                      MaterialPageRoute(
+                                        builder:
+                                            (context) =>
+                                                ScreenOrderDetails(
+                                          order: state
+                                                  .orders[
+                                              index],
                                         ),
                                       ),
-                                    ],
+                                    );
+                                  },
+                                  icon: const Icon(
+                                    CupertinoIcons
+                                        .chevron_forward,
+                                    size: 32,
+                                    fill: BorderSide
+                                        .strokeAlignCenter,
                                   ),
                                 ),
                               ),
+                              shape:
+                                  RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius
+                                        .circular(30),
+                              ),
                             ),
-                          );
-                        },
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              );
+                );
+              },
+            ),
+          ),
+        );
       },
     );
   }
